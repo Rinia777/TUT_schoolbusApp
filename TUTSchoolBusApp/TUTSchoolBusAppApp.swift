b11a7a32655b39881898ed7aa04c7f5121176da2
Rinia777 <i0526rin.ichi@gmail.com>
2025-05-13 22:39:19 +0900

    Initial commit

---
+++ TUTSchoolBusApp/TUTSchoolBusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TUTSchoolBusAppApp.swift
+//  TUTSchoolBusApp
+//
+//  Created by Rin Ichikawa on 2025-05-13.
+//
+
+import SwiftUI
+
+@main
+struct TUTSchoolBusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
